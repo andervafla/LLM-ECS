55993364a5d472da800493c0034ce65f1b0050db
Marian Pinzel <mpinze@softserveinc.com>
2025-03-28 16:37:28 +0200

    new setup ecs cluster

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -10,7 +10,7 @@ resource "aws_sns_topic_subscription" "email_subscription" {
 
 resource "aws_cloudwatch_metric_alarm" "alb_unhealthy_host_count" {
   alarm_name          = "[llm]-[test]-[alb]-[high]-[unhealthy-host-count]"
-  alarm_description   = "unhealthy-host-count"
+  alarm_description   = "Unhealthy Host Count is above 0"
   namespace           = "AWS/ApplicationELB"
   metric_name         = "UnHealthyHostCount"
   statistic           = "Average"
@@ -19,16 +19,15 @@ resource "aws_cloudwatch_metric_alarm" "alb_unhealthy_host_count" {
   period              = 300
   evaluation_periods  = 1
   dimensions = {
-    LoadBalancer = aws_lb.main.arn_suffix
-    TargetGroup  = aws_lb_target_group.tg.arn_suffix
+    LoadBalancer = aws_lb.openwebui_alb.arn_suffix
+    TargetGroup  = aws_lb_target_group.openwebui_tg.arn_suffix
   }
   alarm_actions = [aws_sns_topic.alerts_topic.arn]
 }
 
-
 resource "aws_cloudwatch_metric_alarm" "alb_4xx_errors" {
   alarm_name          = "[llm]-[test]-[alb]-[medium]-[4XX-errors]"
-  alarm_description   = "4xx errors"
+  alarm_description   = "4XX errors sum exceeds 50 in 5 minutes"
   namespace           = "AWS/ApplicationELB"
   metric_name         = "HTTPCode_ELB_4XX_Count"
   statistic           = "Sum"
@@ -37,14 +36,14 @@ resource "aws_cloudwatch_metric_alarm" "alb_4xx_errors" {
   period              = 300
   evaluation_periods  = 1
   dimensions = {
-    LoadBalancer = aws_lb.main.arn_suffix
+    LoadBalancer = aws_lb.openwebui_alb.arn_suffix
   }
   alarm_actions = [aws_sns_topic.alerts_topic.arn]
 }
 
 resource "aws_cloudwatch_metric_alarm" "alb_5xx_errors" {
   alarm_name          = "[llm]-[test]-[alb]-[medium]-[5XX-errors]"
-  alarm_description   = "5xx errors"
+  alarm_description   = "5XX errors sum exceeds 10 in 5 minutes"
   namespace           = "AWS/ApplicationELB"
   metric_name         = "HTTPCode_ELB_5XX_Count"
   statistic           = "Sum"
@@ -53,10 +52,11 @@ resource "aws_cloudwatch_metric_alarm" "alb_5xx_errors" {
   period              = 300
   evaluation_periods  = 1
   dimensions = {
-    LoadBalancer = aws_lb.main.arn_suffix
+    LoadBalancer = aws_lb.openwebui_alb.arn_suffix
   }
   alarm_actions = [aws_sns_topic.alerts_topic.arn]
 }
+
 
 resource "aws_cloudwatch_metric_alarm" "db_high_storage" {
   alarm_name          = "[llm]-[test]-[db]-[high]-[storage]"
@@ -123,9 +123,7 @@ resource "aws_cloudwatch_metric_alarm" "webui_cpu_high" {
     ServiceName = aws_ecs_service.webui_service.name
   }
 
-  alarm_actions = [
-    aws_sns_topic.ecs_alarms_topic.arn
-  ]
+  alarm_actions = [aws_sns_topic.alerts_topic.arn]
 }
 
 resource "aws_cloudwatch_metric_alarm" "webui_memory_high" {
@@ -144,9 +142,7 @@ resource "aws_cloudwatch_metric_alarm" "webui_memory_high" {
     ServiceName = aws_ecs_service.webui_service.name
   }
 
-  alarm_actions = [
-    aws_sns_topic.ecs_alarms_topic.arn
-  ]
+  alarm_actions = [aws_sns_topic.alerts_topic.arn]
 }
 
 resource "aws_cloudwatch_metric_alarm" "webui_tasks_low" {
@@ -165,7 +161,5 @@ resource "aws_cloudwatch_metric_alarm" "webui_tasks_low" {
     ServiceName = aws_ecs_service.webui_service.name
   }
 
-  alarm_actions = [
-    aws_sns_topic.ecs_alarms_topic.arn
-  ]
+alarm_actions = [aws_sns_topic.alerts_topic.arn]
 }
